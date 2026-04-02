5926a8b6cdc1cadcc207f2df89d86a4946d9507e
Tonydeveloper369 <negokwai440@gmail.com>
2026-04-02 20:39:45 -0300

    feat: Meus metodos deletar e como isso integracao na Main

--- CRUD_GerencimentoProdutos/src/Main.java
+++ CRUD_GerencimentoProdutos/src/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Produtos produto = new Produtos();
+        Scanner input = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("\n===== Menu =====");
+            System.out.println("4 - Deletar");
+            System.out.println("0 - Sair");
+            opcao = input.nextInt();
+            switch (opcao) {
+                case 4:
+                    System.out.println("Digite o indice do produto: ");
+                    int index = input.nextInt();
+                    produto.deletar(index);
+                case 0
+                    ;
+                    System.out.println("Saindo, valeu ....");
+                    break;
+                default:
+                    System.out.println("Opcao invalida! ");
+
+            }
+        }while (opcao != 0);
+
+        input.close();
+        
+    }
 }
--- CRUD_GerencimentoProdutos/src/Produtos.java
+++ CRUD_GerencimentoProdutos/src/Produtos.java
@@ -1,2 +1,12 @@
 public class Produtos {
+
+    public void deletar(int index) {
+        if (index >= 0 && index < produtos.size()) {
+            produtos.remove(index);
+            System.out.println("Produto removido!");
+        } else {
+            System.out.println("Índice inválido!");
+        }
+    }
+
 }
