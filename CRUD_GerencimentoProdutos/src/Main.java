1700be09e45d84910a83c9783a3c1d402378361b
FIC <fic@info.local>
2026-04-02 21:44:41 -0300

    feat; metodo listar

--- CRUD_GerencimentoProdutos/src/Main.java
+++ CRUD_GerencimentoProdutos/src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Produtos produtos = new Produtos();
+
+        boolean continuarRodando = true;
+
+        do {
+            System.out.println("Bem vindo ao sistema de gerenciamento de Produtos: ");
+            System.out.println("[2] - LISTAR PRODUTOS");
+            System.out.println("[0] - SAIR DO PROGRAMA");
+
+            System.out.println("Informe uma opcao do menu: ");
+            int opcaoUsuario = sc.nextInt();
+            switch (opcaoUsuario){
+
+            case 2:
+                sc.nextLine();
+                System.out.println("PRODUTOS LISTADOS ABAIXO:  ");
+                System.out.println();
+                produtos.ListaProdutos();
+                break;
+
+
+            case 0:
+
+                continuarRodando = false;
+
+                System.out.println("Programa finalizado. Obrigado por Utilizar.");
+            }
+
+    } while (continuarRodando);
+    }
 }
--- CRUD_GerencimentoProdutos/src/Produtos.java
+++ CRUD_GerencimentoProdutos/src/Produtos.java
@@ -1,2 +1,14 @@
-public class Produtos {
+import java.util.ArrayList;
+
+public class Produtos{
+    ArrayList<String> produtos = new ArrayList<>();
+
+    public void ListaProdutos() {
+            System.out.println("Lista");
+
+        for (int i = 0; i <produtos.size(); i++){
+            System.out.println((i + 1) + ". " + produtos.get(i));
+        }
+
+}
 }
