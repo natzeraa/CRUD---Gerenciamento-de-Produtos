b301f91611862f4906e0f329347ba826d676794f
natalia couto <naty.s.couto@gmail.com>
2026-04-02 21:36:03 -0300

    feat: metodos update

--- CRUD_GerencimentoProdutos/src/Main.java
+++ CRUD_GerencimentoProdutos/src/Main.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Produtos produto = new Produtos();
+
+        boolean continuarRodando = true;
+
+        do{
+            System.out.println("Menu");
+            System.out.println("[3] - ATUALIZAR PRODUTO");
+            System.out.println("[0] - SAIR DO PROGRAMA");
+
+            System.out.println("Informe uma opcao do menu: ");
+            int opcaoUsuario = sc.nextInt();
+
+            switch(opcaoUsuario){
+                case 3 :
+                    // produto.ListarProdutos();
+                    System.out.println("Qual produto deseja atualizar, digite abaixo::");
+                    int indice = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Informe o novo nome do livro:  ");
+                    String novoNome = sc.nextLine();
+
+                    produto.Atualizar(indice -1, novoNome);
+                    break;
+
+                case 0:
+                    continuarRodando = false;
+                    System.out.println("Programa finalizado. Obrigado por Utilizar.");
+            }
+        } while (continuarRodando);
+    }
 }
--- CRUD_GerencimentoProdutos/src/Produtos.java
+++ CRUD_GerencimentoProdutos/src/Produtos.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Produtos {
+
+    ArrayList<String> produtos = new ArrayList<>();
+
+    public void Atualizar (int posicao, String novoNome) {
+
+        if(posicao < 0 || posicao >= produtos.size()){
+            System.out.println("Indice inválido");
+        }
+
+        String valorAntigo = produtos.get(posicao);
+
+        produtos.set(posicao, novoNome);
+
+        System.out.printf("Prodeuto '%s' atualizado para '%s' :", valorAntigo, novoNome);
+    }
 }
