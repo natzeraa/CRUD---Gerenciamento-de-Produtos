d3570bb0ec215af369d7eedb0ed95b16b20090cb
Arthur Santos <arthur.s.carvalho@edu.senai.br>
2026-04-02 21:32:03 -0300

    Feature: Método de Cadastro do produto

--- CRUD_GerencimentoProdutos/src/Main.java
+++ CRUD_GerencimentoProdutos/src/Main.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Produtos gProdutos = new Produtos();
+        boolean continuarRodando = true;
+        do {
+            System.out.println("Bem vindo ao sistema de gerenciamento de Produtos! ");
+            System.out.println("[1] - Cadastrar Produto");
+
+            System.out.println("Informe uma opção do menu:");
+            int opcaoUsuario = sc.nextInt();
+
+            switch (opcaoUsuario){
+                case 1:
+                    sc.nextLine();
+                    System.out.println("Bem vindo ao cadastro de Produtos!");
+                    System.out.print("Informe o nome do Produto: ");
+                    String nomeProduto = sc.nextLine();
+
+                    gProdutos.CadastrarProduto(nomeProduto);
+                    break;
+            }
+
+        }while (continuarRodando);
+        sc.close();
+    }
 }
+
--- CRUD_GerencimentoProdutos/src/Produtos.java
+++ CRUD_GerencimentoProdutos/src/Produtos.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Produtos {
+
+    ArrayList<String> listaProdutos = new ArrayList<>();
+
+    public void CadastrarProduto(String nomeLivro){
+        try {
+            listaProdutos.add(nomeLivro);
+            System.out.printf("O Produto %s foi cadastrado com sucesso!\n", nomeLivro);
+        } catch (Exception error) {
+            System.out.println("Erro ao cadastrar produto: " + error);
+        }
+
+    }
 }
+
